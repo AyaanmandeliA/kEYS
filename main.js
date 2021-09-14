6bf34e7ab7a5051098c66c2135f7c8aaa0b1998c
AyaanmandeliA <78735976+AyaanmandeliA@users.noreply.github.com>
2021-09-14 15:41:49 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -34,17 +34,17 @@ function my_keydown(e) {
 		document.getElementById("d1").innerHTML = "You Pressed Alphabet Key";
 		console.log("alphabet key");
 	}
-	if ((keyPressed >= 37 && keyPressed <= 40)) {
+	else if ((keyPressed >= 37 && keyPressed <= 40)) {
 		arrowkey();
 		document.getElementById("d1").innerHTML = "You Pressed Arrow Key";
 		console.log("arrow key");
 	}
-	if ((keyPressed >= 48 && keyPressed <= 57)) {
+	else if ((keyPressed >= 48 && keyPressed <= 57)) {
 		numberkey();
 		console.log("number_key");
 		document.getElementById("d1").innerHTML = "You pressed Number Key";
 	}
-	if ((keyPressed == 17 || keyPressed == 18  || keyPressed == 27)) {
+	else if ((keyPressed == 17 || keyPressed == 18  || keyPressed == 27)) {
 		specialkey();
 		document.getElementById("d1").innerHTML = "You Pressed Special Key";
 		console.log("Special key");
